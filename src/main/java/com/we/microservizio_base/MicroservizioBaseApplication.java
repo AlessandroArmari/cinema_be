1738dfc662010ee7be626462ec73f62c41d0ee20
a.armari <a.armari@we-com.it>
2025-07-14 16:13:06 +0200

    first commit

---
+++ src/main/java/com/we/microservizio_base/MicroservizioBaseApplication.java
@@ -0,0 +1,13 @@
+package com.we.microservizio_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservizioBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservizioBaseApplication.class, args);
+    }
+
+}

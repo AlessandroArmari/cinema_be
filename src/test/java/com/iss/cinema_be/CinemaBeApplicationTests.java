94897a76143808f7630fc82864592fcbf8d749a3
a.armari <a.armari@we-com.it>
2025-09-23 08:59:28 +0200

    first commit

---
+++ src/test/java/com/iss/cinema_be/CinemaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iss.cinema_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
